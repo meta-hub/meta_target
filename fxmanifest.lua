3b43e220b947380337296438fbec67b2dccbc68d
meta-hub <56391287+meta-hub@users.noreply.github.com>
2022-02-23 17:54:31 +1030

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,10 @@
-fx_version 'adamant'
-game 'rdr3'
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'https://github.com/meta-hub
+github 'https://github.com/meta-hub/meta-target
+version '1.0.0'
+
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 lua54 'yes'

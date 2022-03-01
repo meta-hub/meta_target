a135aa8ebeeb09992ad063a66ad1cbb43acf6cef
meta-hub <56391287+meta-hub@users.noreply.github.com>
2022-03-01 11:52:06 +1030

    Update target.lua

--- lib/target.lua
+++ lib/target.lua
@@ -1,9 +1,9 @@
-local apiRef = exports['bl-target']:getExportNames()
+local apiRef = exports.meta_target:getExportNames()
 
 target = {}
 
 for _,name in ipairs(apiRef) do
   target[name] = function(...)
-    exports['bl-target'][name](exports['bl-target'],...)
+    exports.meta_target[name](exports['bl-target'],...)
   end
 end

6019c16ad6da65e11eeb11be892cf5b55ac73f39
meta-hub <56391287+meta-hub@users.noreply.github.com>
2022-03-23 00:53:57 +1030

    Add files via upload

---
+++ client/compat/bttarget.lua
@@ -0,0 +1,69 @@
+local exportPrefix = 'bt-target'
+
+local function getExportEventName(name)
+  return string.format('__cfx_export_%s_%s',exportPrefix,name)
+end
+
+local exports = {
+  AddCircleZone = function(name,center,radius,options,targetOptions)
+    local items = {}
+
+    for _,t in ipairs(targetOptions.options) do
+      table.insert(items,{
+        label = t.label,
+        onSelect = t.event
+      })
+    end
+
+    return mTarget.addPoint(name,name:upper(),targetOptions.options[1].icon,center,radius,false,items)
+  end,
+
+  AddBoxZone = function(name,center,length,width,options,targetOptions)
+    local items = {}
+
+    for _,t in ipairs(targetOptions.options) do
+      table.insert(items,{
+        label = t.label,
+        onSelect = t.event
+      })
+    end
+
+    return mTarget.addInternalBoxZone(name,name:upper(),targetOptions.options[1].icon,center,length,width,options,targetOptions.distance or false,false,items)
+  end,
+
+  AddPolyZone = function(name,points,options,targetOptions)
+    local items = {}
+
+    for _,t in ipairs(targetOptions.options) do
+      table.insert(items,{
+        label = t.label,
+        onSelect = t.event
+      })
+    end
+
+    return mTarget.addInternalPoly(name,name:upper(),targetOptions.options[1].icon,points,options,targetOptions.distance or false,false,items)
+  end,
+
+  AddTargetModel = function(models,targetOptions)
+    local items = {}
+
+    for _,t in ipairs(targetOptions.options) do
+      table.insert(items,{
+        label = t.label,
+        onSelect = t.event
+      })
+    end
+
+    return mTarget.addModels(name,name:upper(),targetOptions.options[1].icon,models,targetOptions.distance or false,false,items)
+  end,
+
+  RemoveZone = function(name)
+    return mTarget.remove(name)
+  end
+}
+
+for k,v in pairs(exports) do
+  AddEventHandler(getExportEventName(k),function(cb)
+    cb(v)
+  end)
+end
---
+++ client/compat/fivemtarget.lua
@@ -0,0 +1,49 @@
+local exportPrefix = 'fivem-target'
+
+local function getExportEventName(name)
+  return string.format('__cfx_export_%s_%s',exportPrefix,name)
+end
+
+local function onSelect(opts)
+  return function(targetData,itemData,entHit)
+    print('CeeBee',targetData.id,itemData.name,targetData.vars,entHit)
+    opts.onInteract(targetData.id,itemData.name,targetData.vars or {},entHit)
+  end
+end
+
+local exports = {
+  AddTargetEntity = function(opts)
+    return mTarget.addNetEnt(opts.name,opts.label,opts.icon,opts.netId,opts.interactDist or false,onSelect(opts),opts.options,opts.vars) 
+  end,
+
+  AddTargetLocalEntity = function(opts)
+    print(json.encode(opts,{indent = true}))
+    return mTarget.addLocalEnt(opts.name,opts.label,opts.icon,opts.entId,opts.interactDist or false,onSelect(opts),opts.options,opts.vars)
+  end,
+
+  AddTargetPoint = function(opts) 
+    return mTarget.addPoint(opts.name,opts.label,opts.icon,opts.point,opts.interactDist or false,onSelect(opts),opts.options,opts.vars)
+  end,
+
+  AddTargetModel = function(opts) 
+    return mTarget.addModel(opts.name,opts.label,opts.icon,opts.model,opts.interactDist or false,onSelect(opts),opts.options,opts.vars)
+  end,
+
+  AddTargetModels = function(opts) 
+    return mTarget.addModels(opts.name,opts.label,opts.icon,opts.models,opts.interactDist or false,onSelect(opts),opts.options,opts.vars)
+  end,
+
+  AddPolyZone = function(opts)
+    return mTarget.addExternalPoly(opts.name,opts.label,opts.icon,opts.interactDist or false,onSelect(opts),opts.options,opts.vars)
+  end,
+
+  RemoveTargetPoint = function(name)
+    return mTarget.remove(name)
+  end
+}
+
+for k,v in pairs(exports) do
+  AddEventHandler(getExportEventName(k),function(cb)
+    cb(v)
+  end)
+end

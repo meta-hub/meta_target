d07d4924002500306c114ddb042101ef8775dc4b
Taavi <97351942+Taavi-AU@users.noreply.github.com>
2023-01-18 12:03:09 +0800

    remove RedM support

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,5 @@
 fx_version 'adamant'
-game {'gta5','rdr3'}
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'gta5'
 
 github 'https://github.com/meta-hub/meta-target'
 version '0.9.0'

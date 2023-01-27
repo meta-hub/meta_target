fe40cf97fe1ef76ea9c2989b8f3aa371c8e1f42b
Taavi <97351942+Taavi-AU@users.noreply.github.com>
2023-01-27 13:44:50 +0800

    readd redm support

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version 'adamant'
-game 'gta5'
+game {'gta5','rdr3'}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 github 'https://github.com/meta-hub/meta-target'
 version '0.9.0'
